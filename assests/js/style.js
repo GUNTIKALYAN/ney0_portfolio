48bbc35b02a956f980559bf446bda6a52a5a8c60
kalyan <kalyansagar7008@gmail.com>
2025-06-20 21:23:49 +0530

    Add interactive behavior for the list items

--- assests/js/style.js
+++ assests/js/style.js
@@ -5,3 +5,26 @@ navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('open');
 });
 
+
+
+const sections = document.querySelectorAll("section[id]");
+  const navLinks = document.querySelectorAll(".nav__link");
+
+  window.addEventListener("scroll", () => {
+    let scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+      const sectionTop = current.offsetTop - 100;
+      const sectionHeight = current.offsetHeight;
+      const sectionId = current.getAttribute("id");
+
+      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+        navLinks.forEach(link => {
+          link.classList.remove("active-link");
+          if (link.getAttribute("href") === `#${sectionId}`) {
+            link.classList.add("active-link");
+          }
+        });
+      }
+    });
+  });
